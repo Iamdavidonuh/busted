ee68cc5f9bd6cf94886407a051930fa69c36555a
David Onuh <davidonuh1@gmail.com>
2024-01-27 02:20:07 +0100

    add Fake crud methods

--- src/configurations.rs
+++ src/configurations.rs
@@ -1,5 +1,5 @@
-use std::fmt::format;
 use config;
+// use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::postgres::{PgConnectOptions, PgSslMode};
 
 #[derive(serde::Deserialize)]
@@ -8,55 +8,66 @@ pub struct Settings {
     pub application: ApplicationSettings,
 }
 
-
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    // #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
-
 
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
     pub password: String,
+    // #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
-    require_ssl: bool
+    require_ssl: bool,
 }
 
-impl DatabaseSettings{
-    pub fn without_db(&self) -> PgConnectOptions{
-       let ssl_mode = if self.require_ssl {PgSslMode::Prefer} else {PgSslMode::Disable};
-       PgConnectOptions::new()
-       .ssl_mode(ssl_mode)
-       .host(&self.host)
-       .password(&self.password)
-       .port(self.port)
-       .username(&self.username)
+impl DatabaseSettings {
+    pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Prefer
+        } else {
+            PgSslMode::Disable
+        };
+        PgConnectOptions::new()
+            .ssl_mode(ssl_mode)
+            .host(&self.host)
+            .password(&self.password)
+            .port(self.port)
+            .username(&self.username)
     }
 
-    pub fn with_db(&self) -> PgConnectOptions{
+    pub fn with_db(&self) -> PgConnectOptions {
         self.without_db().database(&self.database_name)
     }
-
 }
 
 pub fn get_config() -> Result<Settings, config::ConfigError> {
     let base_path = std::env::current_dir().expect("Could not get current directory");
     let configuration_directory = base_path.join("configuration");
     let environment: Environment = std::env::var("APP_ENVIRONMENT")
-                                            .unwrap_or("local".into())
-                                            .try_into()
-                                            .expect("Invalid environment variable in APP_ENVIRONMENT");
+        .unwrap_or("local".into())
+        .try_into()
+        .expect("Invalid environment variable in APP_ENVIRONMENT");
     let environment_filename = format!("{}.yaml", environment.as_str());
     let settings = config::Config::builder()
-                .add_source(config::File::from(configuration_directory.join("base.yaml")))
-                .add_source(config::File::from(configuration_directory.join(environment_filename)))
-                // APP_APPLICATION__PORT = 5432
-                .add_source(config::Environment::with_prefix("APP").prefix_separator("_").separator("__"))
-                .build()?;
+        .add_source(config::File::from(
+            configuration_directory.join("base.yaml"),
+        ))
+        .add_source(config::File::from(
+            configuration_directory.join(environment_filename),
+        ))
+        // APP_APPLICATION__PORT = 5432
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
+        .build()?;
     settings.try_deserialize::<Settings>()
 }
 
@@ -69,28 +80,28 @@ impl DatabaseSettings {
     }
 }
 
-pub enum Environment{
+pub enum Environment {
     Local,
-    Production
+    Production,
 }
 
-impl Environment{
-    pub fn as_str(&self) -> &'static str{
-        match self{
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
             Environment::Local => "local",
-            Environment::Production => "production"
+            Environment::Production => "production",
         }
     }
 }
 
-impl TryFrom<String> for Environment{
+impl TryFrom<String> for Environment {
     type Error = String;
 
     fn try_from(value: String) -> Result<Self, Self::Error> {
-        match value.to_lowercase().as_str(){
+        match value.to_lowercase().as_str() {
             "local" => Ok(Environment::Local),
             "production" => Ok(Environment::Production),
-            _other => Err(format!("Unsupported environment string"))
+            _other => Err(format!("Unsupported environment string")),
         }
     }
 }
--- src/routes/courses.rs
+++ src/routes/courses.rs
@@ -1,13 +1,64 @@
+use chrono::Utc;
+use serde_json::json;
+use uuid::Uuid;
+
 use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
 
-pub async fn get_all_course(pool : web::Data<PgPool>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-pub async fn create_course(pool : web::Data<PgPool>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct Course {
+    id: Uuid,
+    name: String,
+    author: String,
+    subscribed_at: chrono::DateTime<Utc>,
 }
 
-pub async fn get_course(id: web::Path<u32>, pool : web::Data<PgPool>) -> HttpResponse{
-    HttpResponse::Ok().finish()
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct CreateCourse {
+    name: String,
+    author: String,
+}
+
+pub async fn get_all_course(pool: web::Data<PgPool>) -> HttpResponse {
+    let payload = sqlx::query_as!(
+        Course,
+        r#"SELECT * FROM courses
+        "#,
+    )
+    .fetch_all(pool.get_ref())
+    .await
+    .unwrap();
+    HttpResponse::Ok().json(payload)
+}
+pub async fn create_course(
+    payload: web::Json<CreateCourse>,
+    pool: web::Data<PgPool>,
+) -> HttpResponse {
+    let response = sqlx::query_as!(
+        Course,
+        r#"INSERT INTO courses (id, name, author, subscribed_at)
+        VALUES ($1, $2, $3, $4) returning *;
+        "#,
+        Uuid::new_v4(),
+        payload.name,
+        payload.author,
+        Utc::now(),
+    )
+    .fetch_one(pool.get_ref())
+    .await
+    .unwrap();
+    HttpResponse::Ok().json(response)
+}
+
+pub async fn get_course(id: web::Path<Uuid>, pool: web::Data<PgPool>) -> HttpResponse {
+    let payload = sqlx::query_as!(
+        Course,
+        r#"SELECT * FROM courses WHERE id = $1
+        "#,
+        id.into_inner()
+    )
+    .fetch_one(pool.get_ref())
+    .await
+    .unwrap();
+    HttpResponse::Ok().json(payload)
 }
